4cfc975c7ff8cbb5c7738cb7d99649735558733b
AlecBp <alec.paglia@gmail.com>
2019-01-20 20:22:05 -0500

    posts and user fectch, fix multiple requests

--- blogpost-list/src/reducers/postReducer.js
+++ blogpost-list/src/reducers/postReducer.js
@@ -1,3 +1,12 @@
-export default () => {
-   return 123;
-}
+// default to empty array. First time it is called it will not be undefined
+export default (state = [], action) => {
+	switch (action.type) {
+		case "FETCH_POSTS": {
+			return action.payload;
+		}
+		default: {
+			// Must return state even if no changed, otherwise it returns undefined
+			return state;
+		}
+	}
+};
