192356e206dc347cf319c2946af154ddabea55a4
AlecBp <alec.paglia@gmail.com>
2019-01-23 22:55:33 -0500

    added base to work with Google OAuth2

--- stream-service/client/src/components/GoogleAuth.js
+++ stream-service/client/src/components/GoogleAuth.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 export default class GoogleAuth extends Component {
+	componentDidMount() {
+    // Load "module" you need
+    // https://developers.google.com/api-client-library/javascript/reference/referencedocs
+		window.gapi.load("client:auth2", () => {
+      // Callback function after API is fetched
+      window.gapi.client.init({
+        clientId: "271292356447-1esofbbk3g7gpet3qlhq517okvb1cjl0.apps.googleusercontent.com",
+        scope: "email"
+      })
+		});
+	}
 	render() {
 		return <div>GoogleAuth</div>;
 	}
